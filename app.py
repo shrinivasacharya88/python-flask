ea3c436246a3b44af632ee931a8424cffbe3b944
shrinivasacharya88 <46620420+shrinivasacharya88@users.noreply.github.com>
2020-04-20 13:13:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! this is test from today 20th 2020 and now deployment in kubernaties this is my first cicd"
+    return "Hello World! this is test from today 20th 2020 and now deployment in kubernaties this is my first cicd this include mail"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
