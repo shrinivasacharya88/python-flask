e5f6dbb14a2149d68dbd357cd3f23d3a6c7fa5d5
shrinivasacharya88 <46620420+shrinivasacharya88@users.noreply.github.com>
2020-04-20 11:56:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! this is test flask in new cluster devops 123"
+    return "Hello World! this is test from today"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
